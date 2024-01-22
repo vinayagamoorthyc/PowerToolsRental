0e938a4a567ff9dfbbd8a6b98ed80fbb32f36b29
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-01-22 15:04:39 +0530

    0.0.0.3

--- src/sign_pages/AdminPortal.jsx
+++ src/sign_pages/AdminPortal.jsx
@@ -52,13 +52,13 @@ export default function AdminPortal() {
         <div className='adminnav'>
 
         <div style={{padding:"5px",display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"space-between",zIndex:"1"}}>
-        <a href="/SignIn"><button class="Back_Btn">
-          <a href='/SignIn' class="back_icon"><svg style={{color:"black"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left svg-icon" viewBox="0 0 16 16">
+        <Link to="/SignIn"><button class="Back_Btn">
+          <Link to='/SignIn' class="back_icon"><svg style={{color:"black"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left svg-icon" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
                     <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
-                    </svg></a>
-          <a href='/SignIn' class="back_text">Quit</a>
-        </button></a>
+                    </svg></Link>
+          <Link to='/SignIn' class="back_text">Quit</Link>
+        </button></Link>
         <h3 style={{textAlign:"center",fontFamily:'Montserrat, sans-serif',fontWeight:"bolder",marginTop:"0px",zIndex:"1"}}>Admin Portal</h3>
         <NavDropdown title="Others" id="navbarScrollingDropdown" 
         style={{
