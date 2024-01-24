ab2f39051dba6482708a90e833c4758a6257f742
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-01-24 16:02:41 +0530

    0.0.0.3

--- src/otherPages/ProductPage.jsx
+++ src/otherPages/ProductPage.jsx
@@ -37,11 +37,11 @@ export default function ProductPage() {
   return (
     <div>
       <div style={{background:"black", padding:"5px",position:"fixed",width:"100%",zIndex:"1"}} className='blacknav'>
-        <a href='/'><button class="Back_Btn">
-          <a href='/' class="back_icon"><svg class="svg-icon" viewBox="0 0 20 20">
+        <a href='/AllProducts'><button class="Back_Btn">
+          <a href='/AllProducts' class="back_icon"><svg class="svg-icon" viewBox="0 0 20 20">
 							<path fill="black" d="M8.388,10.049l4.76-4.873c0.303-0.31,0.297-0.804-0.012-1.105c-0.309-0.304-0.803-0.293-1.105,0.012L6.726,9.516c-0.303,0.31-0.296,0.805,0.012,1.105l5.433,5.307c0.152,0.148,0.35,0.223,0.547,0.223c0.203,0,0.406-0.08,0.559-0.236c0.303-0.309,0.295-0.803-0.012-1.104L8.388,10.049z"></path>
 						  </svg></a>
-          <a href='/' class="back_text">Home</a>
+          <a href='/AllProducts' class="back_text">Back</a>
         </button></a>
         </div>
         <div className='pro_view'>
@@ -113,7 +113,7 @@ export default function ProductPage() {
             </Tab>
           </Tabs>
         </div>&nbsp;
-        <Footer/>
+        
     </div>
   )
 }
